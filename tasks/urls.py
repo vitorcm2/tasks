4305a7ae5e371c61a1f71957e7dcf031701c0dd1
vitorcm2 <vitorcm2@al.insper.edu.br>
2020-12-01 10:49:54 -0300

    CLI criada

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('get_tasks', views.get_tasks, name='get_tasks'),
+    path('get_all_tasks', views.get_all_tasks, name='get_all_tasks'),
+    path('get_task/<int:task_id>', views.get_task, name='get_task'),
+    path('edit_task/<int:task_id>', views.edit_task, name='edit_task'),
+    path('delete_task/<int:task_id>', views.delete_task, name='delete_task'),
+    path('create_task', views.create_task, name='create_task'),
 ]
