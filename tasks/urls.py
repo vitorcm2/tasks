76c96aeb3cfb852bce5d0e2b7b4ed2b57b702fb2
vitorcm2 <vitorcm2@al.insper.edu.br>
2020-12-01 18:58:42 -0300

    API_VIEW

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     #path('', views.index, name='index'),
     path('get_all_tasks', views.get_all_tasks, name='get_all_tasks'),
-    path('get_task/<int:task_id>', views.get_task, name='get_task'),
+    #path('get_task/<int:task_id>', views.get_task, name='get_task'),
     path('edit_task/<int:task_id>', views.edit_task, name='edit_task'),
     path('delete_task/<int:task_id>', views.delete_task, name='delete_task'),
     path('create_task', views.create_task, name='create_task'),
