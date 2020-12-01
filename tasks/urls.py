1543f6c756a2c5c3211dd14ac3d7650b0f407ce8
vitorcm2 <vitorcm2@al.insper.edu.br>
2020-12-01 16:54:15 -0300

    Comentando index

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     path('get_all_tasks', views.get_all_tasks, name='get_all_tasks'),
     path('get_task/<int:task_id>', views.get_task, name='get_task'),
     path('edit_task/<int:task_id>', views.edit_task, name='edit_task'),
