7b3c0033436c10ad42550a7365fbf59188a888e5
vitorcm2 <vitorcm2@al.insper.edu.br>
2020-11-30 22:28:07 -0300

    Testando GET Route

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tasks.serializer import TaskSerializer
+from tasks.models import Task
 
 
 # Create your views here.
@@ -7,3 +9,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
+
+
+def get_tasks(request):
+    if request.method == 'GET':
+        tasks = Task.objects.all()
+        tasks_list = TaskSerializer(tasks, many=True)
+        return HttpResponse(tasks_list,content_type="application/json")
